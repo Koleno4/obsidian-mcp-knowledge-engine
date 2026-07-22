b70e850ed57ccc1406ad0809d7b953e6ebe3892c
skwirtl <nhnickolay@gmail.com>
2026-07-22 23:35:07 +0200

    Initial commit: Project structure, runtime configurations, dependencies

---
+++ src/main/java/com/meek/obsidian_second_brain/ObsidianSecondBrainApplication.java
@@ -0,0 +1,13 @@
+package com.meek.obsidian_second_brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsidianSecondBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsidianSecondBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meek/obsidian_second_brain/ObsidianSecondBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meek.obsidian_second_brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsidianSecondBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
